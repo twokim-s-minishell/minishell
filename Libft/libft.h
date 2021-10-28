dc85014a26fb870dcbfceaeef725f7eb3152f589
kk2415 <kkh2415@naver.com>
2021-10-28 16:59:05 +0900

    Debug : << EOF none-commnad here_doc debugging

--- Libft/libft.h
+++ Libft/libft.h
@@ -47,8 +47,6 @@ int					ft_tolower(int c);
 void				*ft_calloc(size_t count, size_t size);
 char				*ft_strdup(const char *src);
 
-int					ft_strcmp(char *s1, char *s2);
-
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
